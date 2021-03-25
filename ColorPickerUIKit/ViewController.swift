5b0621cef855fc6cfadaf0f4ea957e640e365a75
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-25 11:59:32 +1100

    playing around wiht Color Picker in UIKit - changing background color

--- ColorPickerUIKit/ViewController.swift
+++ ColorPickerUIKit/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var selectedColor = UIColor.systemRed
+    private var colorPicker = UIColorPickerViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorPicker.delegate = self
+        view.backgroundColor = selectedColor
+        setupBarButton()
     }
 
-
+    private func selectColor() {
+        colorPicker.supportsAlpha = true
+        colorPicker.selectedColor = selectedColor
+        present(colorPicker, animated: true)
+    }
+    
+    private func setupBarButton() {
+        let pickedColorAction = UIAction(title:"Pick Color") { _ in
+            self.selectColor()
+        }
+        
+        let pickColorBarButton = UIBarButtonItem(image: UIImage(systemName: "eyedropper"), primaryAction: pickedColorAction)
+        
+        navigationItem.rightBarButtonItem = pickColorBarButton
+    }
+    
+}
+extension ViewController: UIColorPickerViewControllerDelegate {
+    func colorPickerViewControllerDidSelectColor(_ viewController:
+                                                    UIColorPickerViewController) {
+        selectedColor = viewController.selectedColor
+        view.backgroundColor = selectedColor
+        
+    }
+    
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        print("Did dismiss controller")
+    }
 }
 
